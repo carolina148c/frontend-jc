e712953893ddc4d6eecfe0f0f9e0f3e0b8425a4b
carolina148c <ac176600@gmail.com>
2025-10-31 19:58:32 -0500

    Creación de archivos principales

---
+++ src/components/TarjetaJuego.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const TarjetaJuego = ({nombre}) => {
+    return (
+        <div>
+            <h1>{nombre}</h1>
+            <h2>hola</h2>
+        </div>
+    )
+}
+
+
+export default TarjetaJuego;
---
+++ src/pages/BibliotecaJuegos.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TarjetaJuego from "../components/TarjetaJuego";
+
+
+const BibliotecaJuegos = () => {
+    return (
+        <div>
+            <TarjetaJuego nombre="Jol"></TarjetaJuego>
+            <TarjetaJuego nombre="Mario"></TarjetaJuego>
+        </div>
+
+    )
+}
+
+export default BibliotecaJuegos;
